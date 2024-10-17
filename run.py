c6378eb38f713f67bf57cad3e720163e66396fca
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-10-17 18:42:43 +0000

    Fix: Correct indentation so message appears at begining of program

--- run.py
+++ run.py
@@ -123,14 +123,14 @@ def main():
     """
     Run all program functions
     """
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_worksheet(sales_data, "sales")
-new_surplus_data = calculate_surplus_data(sales_data)
-update_worksheet(new_surplus_data, "surplus")
-sales_columns = get_last_5_entries_sales()
-stock_data = calculate_stock_data(sales_columns)
-update_worksheet(stock_data, "stock")
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwiches Data Automation")
